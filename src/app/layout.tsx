5b58fdbd7856bcc4730aafc3a516d61d864b4b31
Faishal <faisal@tantowi.com>
2025-11-07 09:14:07 +0700

    fix: change meta property to meta name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,11 +47,11 @@ export default function RootLayout({
         <meta property="og:description" content={description} />
         <meta property="og:image" content={imageUrl} />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content={url} />
-        <meta property="twitter:title" content={title} />
-        <meta property="twitter:description" content={description} />
-        <meta property="twitter:image" content={imageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={imageUrl} />
       </head>
       <body className={inter.className}>
         <div
